c4ce0ce5cda801ac07869b7c2a8e20aa8de2d0ef
Aaditya <aadityapathak18800@gmail.com>
2025-11-17 19:18:57 +0530

    merged updates

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,25 +1,16 @@
-require("dotenv").config();
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: "mysql",
-        logging: false
-    }
-); 
-
-const connectDb = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connected to Railway MySQL using Sequelize!");
-  } catch (error) {
-    console.error("Database connection failed:", error.message);
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false
   }
-};
+);
 
-module.exports = { sequelize, connectDb };
+module.exports = sequelize;
